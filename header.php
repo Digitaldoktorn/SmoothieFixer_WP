fa1675055e5fb98df0fc036955c224ff578df605
Anders Eriksson <digitaldoktorn@gmail.com>
2020-09-24 10:15:28 +0200

    start with crud for members

--- header.php
+++ header.php
@@ -66,16 +66,12 @@ $container = get_theme_mod( 'understrap_container_type' );
               
 
 				<?php if(is_user_logged_in()) { ?>
+					<li><a class="dropdown-item" href="<?php echo esc_url(site_url('/mina-recept')); ?>">Mina recept</a></li>
 					<li><a class="dropdown-item" href="<?php echo wp_logout_url(); ?>">Logout</a></li>
 				<?php } else { ?>
 					<li><a class="dropdown-item" href="<?php echo wp_registration_url(); ?>">Registrera konto</a></li>
 					<li><a class="dropdown-item" href="<?php echo wp_login_url(); ?>">Login</a></li>
 				<?php } ?>
-
-
-
-              
-              
             </ul>
           </li>
         </ul>
